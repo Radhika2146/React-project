1e1229ad8d3041df64df810de040abd700081447
Radhika2146 <103507065+Radhika2146@users.noreply.github.com>
2022-06-10 10:47:33 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About=()=>(
+    <section class="page-section bg-primary text-white mb-0" id="about">
+            <div class="container">
+                <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 ms-auto"><p class="lead">So Hi I am Radhika Mittal a graphic designer my portfolio includes all the graphics made by me!.. </p></div>
+                    <div class="col-lg-4 me-auto"><p class="lead"> So Kindly Have a Look at my work !!!!</p></div>
+                </div>
+                <div class="text-center mt-4">
+                    <a class="btn btn-xl btn-outline-light">
+                        <i class="fas fa-download me-2"></i>
+                    </a>
+                </div>
+            </div>
+        </section>
+)
+
+export default About;
